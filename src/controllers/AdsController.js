58ac43c977f2a19ede3bf0ea45184bc2b08f5204
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-06-07 13:08:56 -0300

    Fim da linha Feito o get para categoriesgit

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -1,5 +1,20 @@
+const Category = require('../models/category')
+
 module.exports = {
     getCategories: async (req, res)=>{
+        const cats = await Category.find()
+
+        let categories = []
+
+        for(let i in cats){
+            categories.push({
+                ...cats[i]._doc,
+                imag:`${process.env.BASE}/assets/images/${cats[i].slug}.png`
+            })
+        }
+
+
+        res.json({categories})
 
     },
     addAction: async (req, res)=>{
